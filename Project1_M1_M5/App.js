dbe8abbec70194418f800aed23a357fa76c30a6b
GilangAddh <gilangadhiperkasa@gmail.com>
2024-05-17 09:40:32 +0700

    Meetings 3 - Flex & Scroll View

--- Project1_M1_M5/App.js
+++ Project1_M1_M5/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import ChallengeScreen from './src/screens/secondScreen';
 // import ChallengeScreen from './src/screens/firstScreen';
+import AddImage from './src/screens/thirdScreen';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <ChallengeScreen />
+      <AddImage />
     </SafeAreaProvider>
   );
 };
---
+++ Project1_M1_M5/src/screens/thirdScreen.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import {View, Text, Image, ScrollView} from 'react-native';
+
+const ThirdScreen = () => {
+  return (
+    <View style={{flex: 1, flexDirection: 'column'}}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 2,
+          justifyContent: 'flex-start',
+          alignItems: 'flex-end',
+        }}>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{
+            uri: 'https://vignette2.wikia.nocookie.net/sonicpokemon/images/7/77/Pikachu.png/revision/latest?cb=20120603213349',
+          }}
+        />
+        <Text style={{color: 'white', fontSize: 18}}>Pikachu</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'orange',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{
+            uri: 'https://i.pinimg.com/originals/d6/61/96/d66196beb60d306a966ea39ed11c2b3d.png',
+          }}
+        />
+        <Text style={{color: 'white', fontSize: 18}}>Bulbasour</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 3,
+          justifyContent: 'flex-end',
+          alignItems: 'flex-start',
+        }}>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{
+            uri: 'https://www.giantbomb.com/a/uploads/scale_medium/13/135472/1892310-078rapidash.png',
+          }}
+        />
+        <Text style={{color: 'white', fontSize: 18}}>Raspidash</Text>
+      </View>
+    </View>
+  );
+};
+
+const AddImage = () => {
+  return (
+    <ScrollView vertical={true}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+        }}>
+        <View style={{margin: 8, padding: 8}}>
+          <Image
+            style={{
+              width: 150,
+              height: 150,
+              borderRadius: 75,
+              borderWidth: 1,
+              borderColor: 'green',
+              backgroundColor: 'aliceblue',
+            }}
+            source={require('../../assets/images/pikachu.png')}
+          />
+        </View>
+        <View style={{margin: 8, padding: 8}}>
+          <Image
+            style={{
+              width: 150,
+              height: 150,
+              borderRadius: 75,
+              borderWidth: 1,
+              borderColor: 'green',
+              backgroundColor: 'aliceblue',
+            }}
+            source={{
+              uri: 'https://i.pinimg.com/originals/30/b5/9b/30b59b7f50c71bb7d2ce9f0fc1c125f2.png',
+            }}
+          />
+        </View>
+        <View style={{margin: 8, padding: 8}}>
+          <Image
+            style={{
+              width: 150,
+              height: 150,
+              borderRadius: 75,
+              borderWidth: 1,
+              borderColor: 'green',
+              backgroundColor: 'aliceblue',
+            }}
+            source={{
+              uri: 'https://i.pinimg.com/originals/d6/61/96/d66196beb60d306a966ea39ed11c2b3d.png',
+            }}
+          />
+        </View>
+        <View style={{margin: 8, padding: 8}}>
+          <Image
+            style={{
+              width: 150,
+              height: 150,
+              borderRadius: 75,
+              borderWidth: 1,
+              borderColor: 'green',
+              backgroundColor: 'aliceblue',
+            }}
+            source={{
+              uri: 'https://www.giantbomb.com/a/uploads/scale_medium/13/135472/1892310-078rapidash.png',
+            }}
+          />
+        </View>
+        <View style={{margin: 8, padding: 8}}>
+          <Image
+            style={{
+              width: 150,
+              height: 150,
+              borderRadius: 75,
+              borderWidth: 1,
+              borderColor: 'green',
+              backgroundColor: 'aliceblue',
+            }}
+            source={{
+              uri: 'https://vignette.wikia.nocookie.net/nintendo/images/3/32/Psyduck.png/revision/latest?cb=20160509232356&path-prefix=en',
+            }}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default AddImage;
