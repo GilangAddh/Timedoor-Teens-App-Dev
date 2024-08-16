abae71609d755c1d72f8da55de6496fa091dfc8d
GilangAddh <gilangadhiperkasa@gmail.com>
2024-08-16 17:19:47 +0700

    Meetings 23 & 24 - Exam

---
+++ SocialMedia/data/Data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    id: 1,
+    imageLink: 'https://wallpaperaccess.com/full/3926232.jpg',
+    username: 'bryan',
+    caption: 'Some people feel the rain, others just get wet',
+    profilePicture:
+      'https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    id: 2,
+    imageLink: 'https://wallpaperaccess.com/full/2259371.jpg',
+    username: 'clara',
+    caption: 'Spring: a lovely reminder of how beautiful change can truly be',
+    profilePicture:
+      'https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    id: 3,
+    imageLink: 'https://wallpaperaccess.com/full/2123210.jpg',
+    username: 'lana',
+    caption: 'Come fly with me',
+    profilePicture:
+      'https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    id: 4,
+    imageLink: 'https://wallpaperaccess.com/full/4004182.jpg',
+    username: 'charlie',
+    caption: 'Better to travel than to arrive',
+    profilePicture:
+      'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    id: 5,
+    imageLink: 'https://wallpaperaccess.com/full/1124115.jpg',
+    username: 'ariana',
+    caption: 'Okay Mother Nature, you win',
+    profilePicture:
+      'https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+];
--- SocialMedia/src/screens/HomeScreen.js
+++ SocialMedia/src/screens/HomeScreen.js
@@ -1,12 +1,138 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from 'react-native';
 import React from 'react';
+import {useState, useEffect} from 'react';
+import {Icon} from 'react-native-elements';
+import {data} from '../../data/Data';
 
 const HomeScreen = () => {
+  const [modifiedData, setModifiedData] = useState([]);
+
+  const likePhoto = (id, likeStatus) => {
+    const likeStatusChanged = modifiedData.map(item => {
+      if (item.id === id) {
+        item.likeStatus = !likeStatus;
+      }
+      return item;
+    });
+    setModifiedData(likeStatusChanged);
+  };
+
+  useEffect(() => {
+    const newData = data.map(item => {
+      item.likeStatus = false;
+      return item;
+    });
+    setModifiedData(newData);
+  }, []);
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <FlatList
+        data={modifiedData}
+        keyExtractor={item => item.id}
+        contentContainerStyle={styles.flatListContainer}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.mainContainer}>
+              <View style={styles.topContainer}>
+                <Image
+                  style={styles.profilePicture}
+                  source={{uri: item.profilePicture}}
+                />
+                <View style={styles.normalMarginLeft}>
+                  <Text style={styles.text}>{item.username}</Text>
+                </View>
+              </View>
+              <Image style={styles.image} source={{uri: item.imageLink}} />
+              <View style={styles.captionContainer}>
+                <Text style={styles.text}>
+                  {item.username}{' '}
+                  <Text style={styles.caption}>{item.caption}</Text>
+                </Text>
+              </View>
+              <View style={styles.bottomContainer}>
+                <TouchableOpacity
+                  style={styles.horizontalContainer}
+                  onPress={() => likePhoto(item.id, item.likeStatus)}>
+                  <Icon
+                    name={item.likeStatus ? 'heart' : 'heart-o'}
+                    type="font-awesome"
+                    size={20}
+                    color={item.likeStatus ? 'red' : null}
+                  />
+                  <View style={styles.normalMarginLeft}>
+                    {item.likeStatus ? <Text>Unlike</Text> : <Text>Like</Text>}
+                  </View>
+                </TouchableOpacity>
+                <View style={styles.horizontalContainer}>
+                  <Icon name="comments-o" type="font-awesome" size={20} />
+                  <View style={styles.normalMarginLeft}>
+                    <Text>Comment</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  flatListContainer: {
+    padding: 8,
+  },
+  mainContainer: {
+    backgroundColor: 'white',
+    margin: 8,
+    borderRadius: 10,
+  },
+  topContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 8,
+  },
+  profilePicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+  },
+  normalMarginLeft: {
+    marginLeft: 8,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  captionContainer: {
+    margin: 8,
+    flex: 1,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  caption: {
+    fontWeight: 'normal',
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  horizontalContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
 
 export default HomeScreen;
